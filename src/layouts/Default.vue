300aa6f87012530d14d23bef89f1868ceda6705f
lahcen alhiane <alhyanelahsen1998@gmail.com>
2021-09-01 12:02:33 +0100

    Update Default.vue

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -106,7 +106,7 @@ export default {
   mounted() {
     this.themeLoad();
     this.overlay = false;
-      document.head.innerHTML = document.head.innerHTML + '<script data-ad-client="ca-pub-4071676546371011" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>';
+      document.head.innerHTML = document.head.innerHTML + "<script data-ad-client="ca-pub-4071676546371011" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>";
 
   },
   methods: {
